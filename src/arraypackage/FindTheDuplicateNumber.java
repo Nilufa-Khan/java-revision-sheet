7073d99745a787f360d4295f22fe5d1cc6462ace
Nilufa Khan <khannilufa5@gmail.com>
2022-11-26 22:28:47 +0530

    The  Duplicate  Number of a given array

--- src/arraypackage/FindTheDuplicateNumber.java
+++ src/arraypackage/FindTheDuplicateNumber.java
@@ -23,6 +23,7 @@ public class FindTheDuplicateNumber {
         int result = 0;
 
         for (int i = 0; i < arr.length-1; i++) {
+
             if((arr[i] ^ arr[i + 1]) == 0){
                 result = arr[i];
             }
