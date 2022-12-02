e6244fc227a7e7058f4cf0802d9ea02fc82542d0
Nilufa Khan <khannilufa5@gmail.com>
2022-12-02 23:53:50 +0530

    minimum number of jumps

--- src/arraypackage/MinNumberOfJumps.java
+++ src/arraypackage/MinNumberOfJumps.java
@@ -6,6 +6,7 @@ import static java.util.Arrays.sort;
 
 public class MinNumberOfJumps {
     public static void main(String[] args) {
+
         MinNumberOfJumps minNumberOfJumps = new MinNumberOfJumps();
         int arr[] = { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
         int n = arr.length;
