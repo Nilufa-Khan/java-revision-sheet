4847542ed7f044d44bbd2506b9f7bc8d26ca15f1
Nilufa Khan <khannilufa5@gmail.com>
2022-12-25 00:20:13 +0530

    Selection sort

--- src/sorting_algo/SelectionSort.java
+++ src/sorting_algo/SelectionSort.java
@@ -1,4 +1,25 @@
 package sorting_algo;
 
 public class SelectionSort {
+    public static void main(String[] args) {
+
+    }
+    public static void selectionSort(int[] arr){
+
+        for (int i = 0; i <arr.length ; i++) {
+            // minimum element find
+            int min_index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[min_index] > arr[j]){
+                    min_index = j;
+
+                }
+//                Swap the found minimum element with the first element
+                int temp = arr[min_index];
+                arr[min_index] = arr[i];
+                arr[i] = temp;
+            }
+
+        }
+    }
 }
