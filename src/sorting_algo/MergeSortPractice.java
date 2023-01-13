0f63b32bcda6d48f5686c787d8a9eefa629fed5a
Nilufa Khan <khannilufa5@gmail.com>
2023-01-13 10:25:01 +0530

    merge sort

--- src/sorting_algo/MergeSortPractice.java
+++ src/sorting_algo/MergeSortPractice.java
@@ -9,27 +9,32 @@ public class MergeSortPractice {
         }
     }
     public static void merge(int[] arr, int start , int end){
+//        finding mid of the array
         int mid =  (start + end)/2;
-
+// declaring and initializing length of the two array
         int len1 = mid - start + 1;
         int len2 = end - mid;
-//        System.out.println("len2 = " + len2);
-//        System.out.println("len1 = " + len1);
-//
-//        System.out.println("mid = " + mid);
+// declaring two array
+
         int firstArray[] = new int[len1];
         int secArray[] = new int[len2];
-
+// copy paste values from the main array to the newly created array
         for (int i = 0; i <len1 ; ++i) {
         firstArray[i] = arr[start + i];
         }
+        // copy paste values from the main array to the newly created array
         for (int i = 0; i <len2 ; ++i) {
             secArray[i] = arr[mid+1 + i];
         }
+//        initial index of two sub arrays
         int initial_index1 = 0;
         int initial_index2 = 0;
+//        initial index of merged array
+
         int ini_indx_merge = start;
+//        checking the condition whether the starting index of each array is less than ending index or not
         while ( initial_index1 < len1 &&  initial_index2 < len2){
+//            checking each arrays value whether its small or equals
             if(firstArray[initial_index1] <= secArray[initial_index2]){
                 arr[ini_indx_merge] = firstArray[initial_index1];
                 initial_index1++;
@@ -57,9 +62,10 @@ public class MergeSortPractice {
         if(start < end){
 //            find middle
             int mid = (start+end)/2;
+//            sort the first and second half of the array
             sort(arr,start,mid);
             sort(arr,mid+1,end);
-
+//merge the sorted halves
             merge(arr,start,end);
         }
     }
