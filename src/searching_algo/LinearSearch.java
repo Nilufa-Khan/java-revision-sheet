0213bd1817f096b796fb25fc7f63aa8dd2a589e7
Nilufa Khan <khannilufa5@gmail.com>
2023-01-14 20:30:45 +0530

    linear search

--- src/searching_algo/LinearSearch.java
+++ src/searching_algo/LinearSearch.java
@@ -1,4 +1,20 @@
 package searching_algo;
 
 public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 80, 30, 60, 50,110, 100, 130, 170};
+        int elementIndex = linear(arr,175);
+        System.out.println("elementIndex = " + elementIndex);
+        int elementIndex1 = linear(arr,110);
+        System.out.println("elementIndex1 = " + elementIndex1);
+    }
+    public static int linear(int[] arr, int searchElement){
+        int index = -1;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == searchElement){
+                index = i;
+            }
+        }
+        return  index;
+    }
 }
