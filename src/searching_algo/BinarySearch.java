dccfa9acd31712f2657b406bc5d38371c3ae2ff2
Nilufa Khan <khannilufa5@gmail.com>
2023-01-14 21:05:25 +0530

    binary  search recursive approach

--- src/searching_algo/BinarySearch.java
+++ src/searching_algo/BinarySearch.java
@@ -3,10 +3,18 @@ package searching_algo;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 50, 60, 80, 110, 130, 140, 170};
+//        Iterative approach
+        System.out.println("Iterative approach");
         int indexOfTheElement = binary(arr,0, arr.length-1,110);
         System.out.println("indexOfTheElement = " + indexOfTheElement);
         int indexOfTheElement1 = binary(arr,0, arr.length-1,175);
         System.out.println("indexOfTheElement1 = " + indexOfTheElement1);
+//        Recursive approach
+        System.out.println("Recursive approach");
+        int indexOfTheElement2 = binaryRecursiveSearch(arr,0, arr.length-1,110);
+        System.out.println("indexOfTheElement = " + indexOfTheElement2);
+        int indexOfTheElement3 = binaryRecursiveSearch(arr,0, arr.length-1,175);
+        System.out.println("indexOfTheElement1 = " + indexOfTheElement3);
     }
     // solve the problem using iterative approach
     public static  int binary(int[] arr , int low , int high, int searchElement){
@@ -26,5 +34,22 @@ public class BinarySearch {
             }
         }
        return index;
+    }
+
+//    solve the problem using recursive approach
+    public static int binaryRecursiveSearch(int[] arr , int low , int high, int searchElement){
+
+        if(low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == searchElement) {
+                return mid;
+            } else if (arr[mid] < searchElement) {
+              return   binaryRecursiveSearch(arr, mid + 1, high, searchElement);
+            } else {
+               return binaryRecursiveSearch(arr, low, mid - 1, searchElement);
+            }
+        }
+
+        return -1;
     }
 }
