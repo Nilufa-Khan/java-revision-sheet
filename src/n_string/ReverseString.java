0f97d6ffe7d8fdc52d77f6d7b53724c012a45e37
Nilufa Khan <khannilufa5@gmail.com>
2023-01-18 20:56:24 +0530

    for you

---
+++ src/n_string/ReverseString.java
@@ -0,0 +1,20 @@
+package n_string;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str[] = "tukda ka jigar mera ho chand mera  Z, you love I".split(" ");
+        String ans = "";
+        for (int i = str.length - 1; i >= 0 ; i--) {
+            ans += str[i] + " ";
+
+        }
+
+
+
+        System.out.println(ans.substring(0, ans.length()-1));
+    }
+
+
+
+
+}
