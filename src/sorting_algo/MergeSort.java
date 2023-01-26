bd2642ef5fd99e15eca3a22326f4454ebe7417f8
Nilufa Khan <khannilufa5@gmail.com>
2023-01-26 23:58:53 +0530

    Interface

--- src/sorting_algo/MergeSort.java
+++ src/sorting_algo/MergeSort.java
@@ -12,11 +12,12 @@ public class MergeSort {
     }
     void merge(int arr[], int l, int m, int r)
     {
+        
         // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
-        
+
         /* Create temp arrays */
         int L[] = new int[n1];
         int R[] = new int[n2];
