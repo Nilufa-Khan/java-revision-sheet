2ecf43271a8b7bc0448e2f81c0220c0731ad76f4
Nilufa Khan <khannilufa5@gmail.com>
2023-01-28 23:46:04 +0530

    reverse digit

---
+++ src/inv_questions/ReverseDigit.java
@@ -0,0 +1,19 @@
+package inv_questions;
+
+public class ReverseDigit {
+    public static void main(String[] args) {
+
+       int rev = reverseDigit(1564);
+        System.out.println("Reverse  = " + rev);
+    }
+//    1564      4651
+    public static  int reverseDigit(int number){
+        int reverse = 0;
+        while (number > 0){
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number /10;
+        }
+        return reverse;
+    }
+}
