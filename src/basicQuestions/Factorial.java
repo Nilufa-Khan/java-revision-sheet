315f818da6f12171f31d25587a287e83909bdc0b
Nilufa Khan <khannilufa5@gmail.com>
2023-02-21 23:52:58 +0530

    factorial

---
+++ src/basicQuestions/Factorial.java
@@ -0,0 +1,32 @@
+package basicQuestions;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = scanner.nextInt();
+        System.out.println("Factorial of "+ number + " = " + fact(number));
+        System.out.println("Do you want to continue ... Enter 1 to continue 0 to exit");
+        int choice = scanner.nextInt();
+        if(choice == 1) {
+        while (choice == 1) {
+
+
+            System.out.println("Factorial of " + number + " = " + fact(number));
+        }
+
+        }
+        System.out.println("Do you want to continue ... Enter 1 to continue 0 to exit");
+        System.out.println("Enter a number");
+        number = scanner.nextInt();
+    }
+    public static int fact(int number){
+        int factorial = 1;
+        for (int i = 1; i <= number ; i++) {
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+}
